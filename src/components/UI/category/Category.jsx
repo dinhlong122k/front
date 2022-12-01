4fe0159f9b4881b14b05d70cfba0907b3a722384
dinhlong2k <nguyendinhlongptit@gmail.com>
2022-12-02 00:34:41 +0700

    t

--- src/components/UI/category/Category.jsx
+++ src/components/UI/category/Category.jsx
@@ -11,24 +11,22 @@ import "../../../styles/category.css";
 
 const Category = () => {
   //const categoryData = useSelector(getCate);
-
-  const [count, setCount] = useState(0);
   const categoryData = useSelector(state => state.categories.categories);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getCate())
+    console.log(categoryData);
   }, [dispatch])
 
   useEffect(() => { 
-    console.log(categoryData);
   }, [categoryData])
   
   return (
     <Container>
       <Row>
-        {categoryData.map((item, index) => (
-          <Col lg="3" md="4" sm="6" xs="6" className="mb-4" key={index}>
+        {categoryData.map((item, id) => {
+          <Col lg="3" md="4" sm="6" xs="6" className="mb-4" key={id}>
             <div className="category__item d-flex align-items-center gap-3">
               <div className="category__img">
                 <img src={item.imgUrl} alt="category__item" />
@@ -36,7 +34,7 @@ const Category = () => {
               <h6>{item.display}</h6>
             </div>
           </Col>
-        ))}
+        })}
       </Row>
     </Container>
   );
--- src/store/shopping-cart/categorySlice.js
+++ src/store/shopping-cart/categorySlice.js
@@ -1,12 +1,11 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-
-const baseUrl = "http://localhost:8002/"
+import { act } from "react-dom/test-utils";
 
 export const getCate = createAsyncThunk(
     "categories/getCate",
     async() =>{
-        const response = await axios.get("http://localhost:3000/categories");
+        const response = await axios.get("http://localhost:8002/category");
         return response.data;
     }
 )
@@ -16,7 +15,8 @@ export const categorySlice = createSlice({
     name: 'categories',
     initialState: {
         categories: [],
-        loading: 'idle'
+        loading: 'idle',
+        size: 0,
     },
     reducers: {
     },
@@ -26,7 +26,8 @@ export const categorySlice = createSlice({
         },
         [getCate.fulfilled] : (state, action) => {
             state.loading = 'success';
-            state.categories = action.payload;
+            state.categories = action.payload.rows;
+            state.size = action.payload.count;
         },
         [getCate.rejected] : (state) => {
             state.loading ='failed';
